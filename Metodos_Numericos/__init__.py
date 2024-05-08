2eb337664d145c16e505cc4cae5c05e0f8b3d94f
Guilherme <72332375+guilhermeMarq@users.noreply.github.com>
2024-05-08 01:31:17 -0300

    Update __init__.py

--- Metodos_Numericos/__init__.py
+++ Metodos_Numericos/__init__.py
@@ -1,3 +1,4 @@
+from Metodos_Numericos.Aproximacao_funcao import *
+from Metodos_Numericos.Autovetores import *
 from Metodos_Numericos.Sistemas_lineares import *
 from Metodos_Numericos.Sistemas_nao_lineares import *
-from Metodos_Numericos.Autovetores import *
